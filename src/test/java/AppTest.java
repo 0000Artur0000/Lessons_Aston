9b88764a228a0d84ae6ab3f7764009a86afd21f8
ArZer0 <artur00artur@yandex.ru>
2024-10-24 06:17:10 +0500

    TestUpdate

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,35 +2,45 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
+    private void runTest(String testName, Runnable testMethod) {
+        System.out.println("[INFO] -------------------------------------------------------");
+        System.out.println("[INFO]  " + testName + " TEST");
+        System.out.println("[INFO] -------------------------------------------------------");
+        testMethod.run();
+    }
 
-        @Test
-        public void testAirport() {
-            com.arzer0.lessons.lesson1.homework.airport.App.main(null);
-        }
-        @Test
-        public void testAutobase() {
-            com.arzer0.lessons.lesson1.homework.autobase.App.main(null);
-        }
-        @Test
-        public void testEntranceExams() {
-            com.arzer0.lessons.lesson1.homework.entrance_exams.App.main(null);
-        }
-        @Test
-        public void testLibrary() {
-            com.arzer0.lessons.lesson1.homework.library.App.main(null);
-        }
-        @Test
-        public void testStore() {
-            com.arzer0.lessons.lesson1.homework.store.App.main(null);
-        }
-        @Test
-        public void testSystemFacultative() {
-            com.arzer0.lessons.lesson1.homework.system_facultative.App.main(null);
-        }
-        @Test
-        public void testTelecom() {
-            com.arzer0.lessons.lesson1.homework.telecom.App.main(null);
-        }
+    @Test
+    public void testAirport() {
+        runTest("AIRPORT", () -> com.arzer0.lessons.lesson1.homework.airport.App.main(null));
+    }
 
+    @Test
+    public void testAutobase() {
+        runTest("AUTOBASE", () -> com.arzer0.lessons.lesson1.homework.autobase.App.main(null));
+    }
 
+    @Test
+    public void testEntranceExams() {
+        runTest("ENTRANCE EXAMS", () -> com.arzer0.lessons.lesson1.homework.entrance_exams.App.main(null));
+    }
+
+    @Test
+    public void testLibrary() {
+        runTest("LIBRARY", () -> com.arzer0.lessons.lesson1.homework.library.App.main(null));
+    }
+
+    @Test
+    public void testStore() {
+        runTest("STORE", () -> com.arzer0.lessons.lesson1.homework.store.App.main(null));
+    }
+
+    @Test
+    public void testSystemFacultative() {
+        runTest("SYSTEM FACULTATIVE", () -> com.arzer0.lessons.lesson1.homework.system_facultative.App.main(null));
+    }
+
+    @Test
+    public void testTelecom() {
+        runTest("TELECOM", () -> com.arzer0.lessons.lesson1.homework.telecom.App.main(null));
+    }
 }
